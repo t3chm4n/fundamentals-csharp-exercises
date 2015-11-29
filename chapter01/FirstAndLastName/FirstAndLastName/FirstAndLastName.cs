90f903a7cccbfb893e5d1e5646cd3be6a8164dd1
maikon neitzel <maikonbr@gmail.com>
2015-11-29 04:21:39 -0200

    Removing unecessary usings

--- chapter01/FirstAndLastName/FirstAndLastName/FirstAndLastName.cs
+++ chapter01/FirstAndLastName/FirstAndLastName/FirstAndLastName.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+Chapter 1 - Exercise 6
+Write a console application that prints your first and last name on the 
+console.
+*/
+
+using System;
 
 namespace FirstAndLastName
 {
